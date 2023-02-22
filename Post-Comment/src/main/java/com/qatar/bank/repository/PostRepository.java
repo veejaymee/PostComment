8a0ec51b7daf6216b31b792d0604e5a9b7c0426b
veejaymee <veejaymee@gmail.com>
2023-02-22 22:54:03 +0200

    Delete Post-Comment directory

--- Post-Comment/src/main/java/com/qatar/bank/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qatar.bank.repository;
-
-import com.qatar.bank.entity.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface PostRepository extends MongoRepository<Post, String>, PostCustomRepository {
-	
-}
--- Post-Comment/src/main/java/com/qatar/bank/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.qatar.bank.repository;
-
-import com.qatar.bank.entity.PostUser;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<PostUser, String>, UserCustomRepository {
-	
-}
